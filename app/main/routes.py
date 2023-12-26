62eab1f48cee6a08a8003b53d1c6bf802b473301
FrozenCell239 <val8ioks@gmail.com>
2023-12-26 18:53:51 +0100

    Added empty password/mail/username verification.

--- app/main/routes.py
+++ app/main/routes.py
@@ -181,6 +181,12 @@ def register():
     elif request.method == 'POST' :
         # Errors handling
         errors = []
+        if request.form['username'] == '' :
+            error.append["Le nom d'utilisateur ne peut pas être vide."]
+        if request.form['mail'] == '' :
+            error.append["L'adresse mail ne peut pas être vide."]
+        if request.form['password'] == '' :
+            error.append["Le mot de passe ne peut pas être vide."]
         if request.form['password'] != request.form['password_confirm'] :
             errors.append("Les mots de passes ne sont pas identiques.")
         if User.query.filter_by(username = request.form['username']).first():
