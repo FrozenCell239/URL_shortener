4806fe4862c69502b0dde51053d07eb359139785
FrozenCell239 <val8ioks@gmail.com>
2024-02-06 18:00:35 +0100

    Fixed a bug due to the pagination feature. (The user was redirected on the first page of its links/files list instead of the first page after toggling or deleting a file/link.)

--- app/user/routes.py
+++ app/user/routes.py
@@ -147,7 +147,7 @@ def toggle_link(link_id : int):
         link = Link.query.filter_by(id = link_id).first()
         link.toggleState()
         db.session.commit()
-        return redirect(url_for('user.links'))
+        return redirect(url_for('user.links', page = request.args.get('back_to', 1, int)))
 
 @user_bp.route('/links/<int:link_id>/delete')
 @login_required
@@ -162,7 +162,7 @@ def delete_link(link_id : int):
     else:
         link = Link.query.filter_by(id = link_id).delete()
         db.session.commit()
-        return redirect(url_for('user.links'))
+        return redirect(url_for('user.links', page = request.args.get('back_to', 1, int)))
 
 @user_bp.route('/files')
 @login_required
@@ -207,7 +207,7 @@ def toggle_file(file_id : int):
         file = File.query.filter_by(id = file_id).first()
         file.toggleState()
         db.session.commit()
-        return redirect(url_for('user.files'))
+        return redirect(url_for('user.files', page = request.args.get('back_to', 1, int)))
 
 @user_bp.route('/files/<int:file_id>/delete')
 @login_required
@@ -232,4 +232,4 @@ def delete_file(file_id : int):
         db.session.commit()
 
         # User's files page display
-        return redirect(url_for('user.files'))
+        return redirect(url_for('user.files', page = request.args.get('back_to', 1, int)))
