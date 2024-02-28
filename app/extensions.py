cd16ee0443c5a6ceeb00159428ecde080fa8ec1e
FrozenCell239 <val8ioks@gmail.com>
2024-02-28 12:39:04 +0100

    Added Memcached or Redis caching options for Limiter.

--- app/extensions.py
+++ app/extensions.py
@@ -9,7 +9,18 @@ bcrypt = Bcrypt()
 # Limiter for bruteforce attacks preventing
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-limiter = Limiter(get_remote_address)
+limiter = Limiter(
+    get_remote_address,
+
+    # Memcached options
+    storage_uri = 'memcached://localhost:11211',
+    storage_options = {}
+
+    # Redis options
+    #storage_uri = 'redis://localhost:6379',
+    #storage_options = {'socket_connect_timeout': 30},
+    #strategy = 'fixed-window' #// or 'moving-window'
+)
 
 # Flask-WTF for CSRF tokens
 from flask_wtf.csrf import CSRFProtect
